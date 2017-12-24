3a07f599fd6bcfaa9ae6e36b60d5e1079179ce87
praddx <praddcell@gmail.com>
2017-12-24 18:31:20 +0300

    HW3. Moved MainTestSortedArrayStorage class from basejava/storage/ to /basejava/.

--- src/ru/javawebinar/basejava/MainTestSortedArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestSortedArrayStorage.java
@@ -1,6 +1,7 @@
-package ru.javawebinar.basejava.storage;
+package ru.javawebinar.basejava;
 
 import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.SortedArrayStorage;
 
 /**
  * Test for com.javawebinar.basejava.storage.ArrayStorage
