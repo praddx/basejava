376af3ff576702153a4609fadf20cb3d8b37cf1b
Alexander <praddcell@gmail.com>
2018-03-11 10:17:44 +0300

    HW 10. Sync with lesson.

--- src/ru/javawebinar/basejava/storage/serializer/StreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/StreamSerializer.java
@@ -1,11 +1,11 @@
-package ru.javawebinar.basejava.storage;
+package ru.javawebinar.basejava.storage.serializer;
 
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.io.*;
 
-public class ObjectStreamPathStorage implements SerializationStrategy {
+public class StreamSerializer implements SerializationStrategy {
 
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
--- test/ru/javawebinar/basejava/storage/ObjectStreamPathStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ObjectStreamPathStorageTest.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.storage.serializer.StreamSerializer;
+
 /**
  * Created by Pradd on 30.12.2017.
  */
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
 
     public ObjectStreamPathStorageTest() {
-        super(new PathStorage(PAHT_STORAGE_DIR, new ObjectStreamPathStorage()));
+        super(new PathStorage(PAHT_STORAGE_DIR, new StreamSerializer()));
     }
 }
